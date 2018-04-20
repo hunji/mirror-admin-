e2722a4d9daae7d335de042e178b5d9b3ba80cb5
hunji <guo194194@gmail.com>
2018-04-20 16:27:50 +0800

    添加基础文件，开始编写登录页面

---
+++ src/views/login/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="site-wrapper site-page--login">
+    <div class="site-content__wrapper">
+      <div class="site-content">
+        <div class="brand-info">
+          <h2 class="brand-info__text">renren-fast-vue</h2>
+          <p class="brand-info__intro">renren-fast-vue基于vue、element-ui构建开发，实现renren-fast后台管理前端功能，提供一套更优的前端解决方案。</p>
+        </div>
+        <div class="login-main">
+          <h3 class="login-title">管理员登录</h3>
+          <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" status-icon>
+            <el-form-item prop="userName">
+              <el-input v-model="dataForm.userName" placeholder="帐号"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+              <el-input v-model="dataForm.password" type="password" placeholder="密码"></el-input>
+            </el-form-item>
+            <el-form-item prop="captcha">
+              <el-row :gutter="20">
+                <el-col :span="14">
+                  <el-input v-model="dataForm.captcha" placeholder="验证码">
+                  </el-input>
+                </el-col>
+                <el-col :span="10" class="login-captcha">
+                  <img :src="captchaPath" @click="getCaptcha()" alt="">
+                </el-col>
+              </el-row>
+            </el-form-item>
+            <el-form-item>
+              <el-button class="login-btn-submit" type="primary" @click="dataFormSubmit()">登录</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
